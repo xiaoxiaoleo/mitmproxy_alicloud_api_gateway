5504bc84f85af8e414f7812786e7388939982b91
小小leo <youfbi@foxmail.com>
2020-07-03 15:28:27 +0800

    Update signreq.py

--- signreq.py
+++ signreq.py
@@ -10,33 +10,49 @@ except ImportError:
      from urlparse import urlparse
 
 
-appKey = "xx"
-appSecret = "xxxxxx"
 
-def signreq(data,aurl):     
+
+def signreq(data,aurl, appKey, appSecret, request_method):   
+    if not isinstance(data, dict):
+        data = json.loads(data)
+    print(20*'-') 
+
+    
     u = urlparse(aurl)
     host = u.scheme + '://' + u.netloc
-    url = u.path
+    url = u.path 
+    if u.query:
+        url = u.path + '?' + u.query
+    print(url)
     timestamp =  DateUtil.get_timestamp()
     Datet = DateUtil.get_rfc_2616_date() 
  
-    accept = '*/*'
-    request_method = "POST"
+    accept = '*/*' 
     xcanonce = UUIDUtil.get_uuid()
-    contentmd5 = md5_tool.get_md5_base64_str(json.dumps(data))
+    contentmd5 = md5_tool.get_md5_base64_str(data)
 
     
     str_to_sign = ''
 
-     
-    headers = {
-    'Content-MD5': contentmd5, 
-    'User-Agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 13_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/7.0.12(0x17000c2d) NetType/WIFI Language/zh_CN',
-    #'Content-Length':'',
-    'Content-Type': 'application/json',
-    "Accept": '*/*',
-    "Date": Datet
-    }
+    if request_method == "POST":
+        headers = {
+        'Content-MD5': contentmd5, 
+        'User-Agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 13_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/7.0.12(0x17000c2d) NetType/WIFI Language/zh_CN',
+        #'Content-Length':'',  =
+        'Content-Type': 'application/json',
+        "Accept": '*/*',
+        "Date": Datet
+        }
+
+        
+    if request_method == "GET":
+        headers = { 
+        'User-Agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 13_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/7.0.12(0x17000c2d) NetType/WIFI Language/zh_CN',
+        #'Content-Length':'',
+        'Content-Type': 'application/json',
+        "Accept": '*/*',
+        "Date": Datet
+        }
 
     headers['x-ca-signaturemethod'] = 'HmacSHA356'
     headers['x-ca-nonce'] = xcanonce
@@ -46,7 +62,7 @@ def signreq(data,aurl):
      
                                                            headers=headers)
 
-
+    #print(str_to_sign)
     xcasignature = sha_hmac256.sign(str_to_sign, appSecret)
     headers['X-Ca-Signature'] = xcasignature
     headers['X-Ca-Signature-headers'] = "x-ca-key,x-ca-nonce,x-ca-signaturemethod"
@@ -59,8 +75,8 @@ def signreq(data,aurl):
     headers['x-ca-signature'] = xcasignature
  
     #print(headers)
-
-    return headers
+    print(20*'-')
+    return (headers,data)
 
 
 if __name__ == '__main__':
